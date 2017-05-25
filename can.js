76ff5631a31c3118293440bda37e5b252b60750a
Canberk Çevik <can@Canberks-MacBook-Pro.local>
2017-05-25 19:02:55 +0300

    Edited Code

--- can.js
+++ can.js
@@ -9,18 +9,18 @@ function exc() {
         }
 
 function lexc() {
-    var lturkish1 = document.getElementByID('ltry1').value;
-    var lbit2 = document.getElementByID('lbtc2').value;
-    var lrates = document.getElementByID('lrate').value;
+    var lturkish1 = document.getElementById('ltry1').value;
+    var lbit2 = document.getElementById('lbtc2').value;
+    var lrates = document.getElementById('lrate').value;
     var lresultb = parseFloat(lturkish1) / parseFloat(lrates);
-    var lresultbS= parseFloat(lresultb.toFixed(3));
+    var lresultbS = parseFloat(lresultb.toFixed(3));
     if (!isNaN(lresultbS)) {
-      document.getElementByID('lbtc1').value = lresultbS;
+      document.getElementById('lbtc1').value = lresultbS;
     }
-    var lresultt = parseFloat(lbit2) . parseFloat(lrates);
-    var lresulttS= parseFloat(lresultt.toFixed(3));
+    var lresultt = parseFloat(lbit2) * parseFloat(lrates);
+    var lresulttS = parseFloat(lresultt.toFixed(3));
     if (!isNaN(lresulttS)) {
-      document.getElementByID('ltry2').value = lresulttS;
+      document.getElementById('ltry2').value = lresulttS;
     }
 
   }
