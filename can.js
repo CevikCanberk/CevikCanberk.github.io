82b872419b1aa6b23cab5442e73e375e05a025f0
Canberk Çevik <can@Canberks-MacBook-Pro.local>
2017-05-25 16:53:30 +0300

    7 -> 3 toFixed

--- can.js
+++ can.js
@@ -2,7 +2,7 @@ function mult() {
             var turkish = document.getElementById('try').value;
             var bitcoin = document.getElementById('btc').value;
             var result =  parseFloat(turkish) /  parseFloat(bitcoin);
-            var resultS = parseFloat(result.toFixed(7))
+            var resultS = parseFloat(result.toFixed(3))
             if (!isNaN(resultS)) {
                 document.getElementById('rate').value = resultS;
             }
