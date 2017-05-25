c40234aa6a360a821228fd11bd195d8b1661dd3f
Canberk Çevik <can@Canberks-MacBook-Pro.local>
2017-05-25 16:25:07 +0300

    Changed Int to Float

--- can.js
+++ can.js
@@ -1,7 +1,7 @@
 function mult() {
             var turkish = document.getElementById('try').value;
             var bitcoin = document.getElementById('btc').value;
-            var result = parseInt(turkish) / parseInt(bitcoin);
+            var result = parseFloat(turkish) / parseFloat(bitcoin);
             if (!isNaN(result)) {
                 document.getElementById('rate').value = result;
             }
