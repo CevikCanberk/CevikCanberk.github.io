e5f12dfbb3aac0540a6eec62a8c102698d4f4580
Canberk Çevik <can@Canberks-MacBook-Pro.local>
2017-05-25 18:50:10 +0300

    New Functions

--- can.js
+++ can.js
@@ -1,4 +1,4 @@
-function mult() {
+function exc() {
             var turkish = document.getElementById('try').value;
             var bitcoin = document.getElementById('btc').value;
             var result =  parseFloat(turkish) /  parseFloat(bitcoin);
@@ -7,3 +7,20 @@ function mult() {
                 document.getElementById('rate').value = resultS;
             }
         }
+
+function lexc() {
+    var lturkish1 = document.getElementByID('ltry1').value;
+    var lbit2 = document.getElementByID('lbtc2').value;
+    var lrates = document.getElementByID('lrate').value;
+    var lresultb = parseFloat(lturkish1) / parseFloat(lrates);
+    var lresultbS= parseFloat(lresultb.toFixed(3));
+    if (!isNaN(lresultbS)) {
+      document.getElementByID('lbtc1').value = lresultbS;
+    }
+    var lresultt = parseFloat(lbit2) . parseFloat(lrates);
+    var lresulttS= parseFloat(lresultt.toFixed(3));
+    if (!isNaN(lresulttS)) {
+      document.getElementByID('ltry2').value = lresulttS;
+    }
+
+  }
